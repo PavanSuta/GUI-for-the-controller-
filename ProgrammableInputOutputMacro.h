0b4c3e091dcf166944cbdd24cd77b96ec79f6cea
PavanSuta <pavansuta.hd@gmail.com>
2015-05-31 18:12:09 -0400

    Create ProgrammableInputOutputMacro.h
    added ProgrammableInputOutputMacro.h file

---
+++ ProgrammableInputOutputMacro.h
@@ -0,0 +1,41 @@
+#ifndef PROGRAMMABLEINPUTOUTPUTMACRO_H
+#define PROGRAMMABLEINPUTOUTPUTMACRO_H
+
+
+# define OkIconLength 100
+# define OkIconBreadth 100
+# define NokIconLength 30
+# define NokIconBreadth 30
+# define HelpIconLength 60
+# define HelpIconBreadth 60
+# define InformationIconLength 28
+# define InformationIconBreadth 28
+# define InsertIconLength 30
+# define InsertIconBreadth 30
+# define DeleteIconLength 30
+# define DeleteIconBreadth 30
+# define ModuleColumn 0
+# define SignalColumn 1
+# define InputColumn 2
+# define OutputColumn 3
+# define OkIconAddress "C:/Users/20043969/Desktop/right.png"
+# define NokIconAddress "C:/Users/20043969/Desktop/wrong.png"
+# define HelpIconAddress "C:/Users/20043969/Desktop/help.png"
+# define InformationIconAddress "C:/Users/20043969/Desktop/info.png"
+# define InsertIconAddress "C:/Users/20043969/Desktop/add.png"
+# define DeleteIconAddress "C:/Users/20043969/Desktop/erase.png"
+# define HelpTextAddress "C:/hello/New.txt"
+# define ZerothColumnX 1
+# define FirstColumnX 1
+# define ThirdColumnX 3
+# define MaxCharLength 20
+# define MaxCharLengthofModule 20
+# define MaxCharLengthofSignal 20
+# define MaxCharLengthofInput 20
+# define MaxCharLengthofOutput 20
+# define MaxSignal 6
+# define GroupSignals "Ext.App.Sel.0"<<"Ext.App.Sel.1"<<"Ext.App.Sel.2"<<"Ext.App.Sel.3"<<"Ext.App.Sel.4"<<"Ext.App.Sel.5";
+# define TMSignals "FINDINI"<<"OTINI"
+
+# define Four 4
+#endif // PROGRAMMABLEINPUTOUTPUTMACRO_H
